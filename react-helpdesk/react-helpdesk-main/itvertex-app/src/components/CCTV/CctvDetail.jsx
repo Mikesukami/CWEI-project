b9e31224c06ad571928f4f55ea31e619aac30260
Mikesukami <mikesu2544@gmail.com>
2024-01-10 19:38:53 +0700

    Update CctvDetail to Form Validated

--- react-helpdesk/react-helpdesk-main/itvertex-app/src/components/CCTV/CctvDetail.jsx
+++ react-helpdesk/react-helpdesk-main/itvertex-app/src/components/CCTV/CctvDetail.jsx
@@ -1,10 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { API_GET } from '../../api';
-import { Card, Form, Row, Col, Button } from 'react-bootstrap';
+import { Card, Row, Col, Button, Form, FormGroup } from 'react-bootstrap';
 
 export default function CctvDetail() {
     let params = useParams();
+
+    const [ipAddress, setIpAddress] = useState("");
+    const [cctvName, setCctvName] = useState("");
+    const [ipcStatus, setIpcStatus] = useState(0);
+    const [ipcStatusName, setIpcStatusName] = useState([]);
+    const [validated, setValidated] = useState(false);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await API_GET("ipc_status_name");
+            // let json = await response.json();
+            console.log(response.data);
+            setIpcStatusName(response.data);         
+            
+        }
+        fetchData();
+    }, []);
+
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         const response = await fetch(
+    //             "http://localhost:4080/api/cctv_read_by_id/" + params.id,
+    //         )
+    //     }
+    // }, []);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+    
+        setValidated(true);
+      };
 
     function cctvCreate(){
 
@@ -17,106 +52,51 @@ export default function CctvDetail() {
                     <Card>
                         <Card.Header as="h5" className="bg-primary text-white">เพิ่มข้อมูล CCTV</Card.Header>
                         <Card.Body>
-                            <Form onSubmit={cctvCreate}>
-                                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-                                    <Form.Label column sm="2">
-                                        ชื่อ <span className="text-danger"> * </span> :
-                                    </Form.Label>
-                                    <Col sm="10">
-                                        <Form.Control type="text" placeholder="กรอกชื่อลูกค้า" />
+                            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                                <FormGroup as={Row} className="mb-3" controlId="formPlaintextPassword">
+                                    <Form.Label column sm="2"> IP Address <span className="text-danger"> * </span> : </Form.Label>
+                                    <Col sm="5"> 
+                                    <Form.Control type="text" 
+                                        value={ipAddress} 
+                                        placeholder="Enter IP Address" 
+                                        onChange={(e) => setIpAddress(e.target.value)} required/>
+                                    <Form.Control.Feedback type="invalid"> Please Enter IP Address</Form.Control.Feedback>
                                     </Col>
-                                </Form.Group>
-                                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-                                    <Form.Label column sm="2">
-                                        นามสกุล <span className="text-danger"> * </span> :
-                                    </Form.Label>
-                                    <Col sm="10">
-                                        <Form.Control type="text" placeholder="กรอกนามสกุลลูกค้า" />
+                                </FormGroup>
+                                {/*CCTV Name */}
+                                <FormGroup as={Row} className="mb-3" controlId="formPlaintextPassword">
+                                    <Form.Label column sm="2"> CCTV Name  <span className="text-danger"> * </span> :</Form.Label>
+                                    <Col sm="5">
+                                        <Form.Control type="text" 
+                                            value={cctvName}
+                                            placeholder="Enter CCTV Name"
+                                            onChange={(e) => setCctvName(e.target.value)} required/>
+                                        <Form.Control.Feedback type="invalid"> Please Enter CCTV Name</Form.Control.Feedback>
                                     </Col>
-                                </Form.Group>
-                                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-                                    <Form.Label column sm="2">
-                                        เบอร์โทร <span className="text-danger"> * </span> :
-                                    </Form.Label>
-                                    <Col sm="10">
-                                        <Form.Control type="text" maxLength={10} placeholder="กรอกเบอร์โทรลูกค้า" />
+                                </FormGroup>
+                                {/*IPC Status */}
+                                <FormGroup as={Row} className="mb-3" controlId="validateIpcStatus">
+                                    <Form.Label column sm="2"> IPC Status <span className="text-danger"> * </span> : </Form.Label>
+                                    <Col sm="5">
+                                        <Form.Select 
+                                        value={ipcStatus} 
+                                        onChange={(e) => setIpcStatus(e.target.value)} 
+                                        required>
+                                            <option label="Select CCTV Status"></option>
+                                            {ipcStatusName.map((item, index) => (
+                                                <option key={index} 
+                                                        value={item.ipc_status}>
+                                                {item.ipc_status_name}
+                                                </option>
+                                            ))
+                                            }
+                                        </Form.Select>
                                     </Col>
-                                </Form.Group>
+                                    <Form.Control.Feedback type="invalid">
+                                        Please select IPC Status.
+                                    </Form.Control.Feedback>
+                                </FormGroup>
                                 <hr />
-                                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-                                    <Form.Label column sm="2">
-                                        บ้านเลขที่ :
-                                    </Form.Label>
-                                    <Col sm="10">
-                                        <Form.Control type="text" placeholder="กรอกบ้านเลขที่ของลูกค้า" />
-                                    </Col>
-                                </Form.Group>
-                                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-                                    <Form.Label column sm="2">
-                                        หมู่ที่ :
-                                    </Form.Label>
-                                    <Col sm="10">
-                                        <Form.Control type="text" placeholder="กรอกหมายเลขหมู่บ้านของลูกค้า" />
-                                    </Col>
-                                </Form.Group>
-                                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-                                    <Form.Label column sm="2">
-                                        ซอย :
-                                    </Form.Label>
-                                    <Col sm="10">
-                                        <Form.Control type="text" placeholder="กรอกซอยของลูกค้า" />
-                                    </Col>
-                                </Form.Group>
-                                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-                                    <Form.Label column sm="2">
-                                        ถนน :
-                                    </Form.Label>
-                                    <Col sm="10">
-                                        <Form.Control type="text" placeholder="กรอกถนนของลูกค้า" />
-                                    </Col>
-                                </Form.Group>
-                                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-                                    <Form.Label column sm="2">
-                                        จังหวัด  :
-                                    </Form.Label>
-                                    <Col sm="10">
-                                        <Form.Control type="text" value="สงขลา" readOnly />
-                                    </Col>
-                                </Form.Group>
-                                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-                                    <Form.Label column sm="2">
-                                        อำเภอ  :
-                                    </Form.Label>
-                                    <Col sm="10">
-                                        <Form.Select aria-label="Default select example">
-                                            <option>Open this select menu</option>
-                                            <option value="1">One</option>
-                                            <option value="2">Two</option>
-                                            <option value="3">Three</option>
-                                        </Form.Select>
-                                    </Col>
-                                </Form.Group>
-                                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-                                    <Form.Label column sm="2">
-                                        ตำบล  :
-                                    </Form.Label>
-                                    <Col sm="10">
-                                        <Form.Select aria-label="Default select example">
-                                            <option>Open this select menu</option>
-                                            <option value="1">One</option>
-                                            <option value="2">Two</option>
-                                            <option value="3">Three</option>
-                                        </Form.Select>
-                                    </Col>
-                                </Form.Group>
-                                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-                                    <Form.Label column sm="2">
-                                        รหัสไปรษณีย์ :
-                                    </Form.Label>
-                                    <Col sm="10">
-                                        <Form.Control type="text" placeholder="กรอกรหัสไปรษณีย์ของลูกค้า" />
-                                    </Col>
-                                </Form.Group>
                                 <Button type="submit" className="sign-btn" style={{background: 'green' , border: '0'}}>บันทึก</Button>
                             </Form>
                         </Card.Body>
