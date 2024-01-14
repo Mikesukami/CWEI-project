cace6c8c011e95e99e0a8171ebc3b0eb6e5b1ef0
Mikesukami <mikesu2544@gmail.com>
2024-01-14 16:00:06 +0700

    update not found page

--- react-helpdesk/react-helpdesk-main/itvertex-app/src/components/NotFound/NotFound.jsx
+++ react-helpdesk/react-helpdesk-main/itvertex-app/src/components/NotFound/NotFound.jsx
@@ -1,7 +1,18 @@
-export default function NotFound() {
-    return(
-        <>
-            <h2>ไม่พบหน้าเว็บ (404 Page Not Found)</h2>
-        </>
-    );
-}
+import React from 'react';
+import img2 from '../asset/image/notfound.jpg'
+
+const NotFoundPage = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <img
+        src={img2} // Replace with the URL of your not found image
+        alt="Not Found"
+        style={{ maxWidth: '30%', height: 'auto', marginTop: '50px' }}
+      />
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for might be unavailable or does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
